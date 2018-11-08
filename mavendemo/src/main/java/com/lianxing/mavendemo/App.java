0e6205c12d318b68eb4a7f56d0b0bb35cb0d5dd5
lianxing <Administrator@4LXG3LURFP6DV98>
2018-11-08 14:32:04 +0800

    eclipse提交

--- mavendemo/src/main/java/com/lianxing/mavendemo/App.java
+++ mavendemo/src/main/java/com/lianxing/mavendemo/App.java
@@ -1,8 +1,13 @@
 package com.lianxing.mavendemo;
 
 /**
+<<<<<<< HEAD
  * Hello world!
  * aaaaaa
+=======
+ * Hello world!  lianxing
+ * aa
+>>>>>>> branch 'master' of https://github.com/lianxing/mavendemo.git
  */
 public class App 
 {
